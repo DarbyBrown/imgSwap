cc4e3f896bb175fd43a702e7b206b61269ddfa69
Hugo Darby-Brown <hugo@darbybrown.com>
2013-09-09 19:44:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#imgSwap.js
+##Super simple responsive images
+
+Will be working on this readme soon
+
+[Article and Demo](http://darbybrown.com/2013/09/02/imgswap-super-simple-responsive-images.html) 
---
+++ js/imgswap.js
@@ -0,0 +1,54 @@
+function checkImage(testUrl) { 
+    var http = jQuery.ajax({
+        type: "HEAD",
+        url: testUrl,
+        async: false
+    });
+
+    if (http.status == 200) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function imgSwap() {
+
+    var images = $('img[data-swap]'); 
+
+
+    for (var i = 0; i < images.length; i++) {
+
+        var imageFormat = images[i].src.substr(-4);
+        var imageName = images[i].src.substr(0, images[i].src.length - 7);
+        var sml = imageName + "sml" + imageFormat;
+        var med = imageName + "med" + imageFormat;
+        var lrg = imageName + "lrg" + imageFormat;
+
+
+        if (window.devicePixelRatio > 1) { 
+            if ($(window).width() > 480 && checkImage(lrg)) {
+                imageName = lrg;
+
+            } else {
+                imageName = med;
+            }
+
+        } else if ($(window).width() > 900 && checkImage(lrg)) {
+            imageName = lrg;
+
+        } else if ($(window).width() > 480) {
+            imageName = med;
+        } else {
+            imageName = sml;
+        }
+
+
+        images[i].src = imageName;
+    }
+
+}
+imgSwap();
+
+$(window).resize(function() { imgSwap(); });
